a61ab80c7e7283cf8348ee06d7c877f6219f4075
Bernd Mayinger <mayingbe@comw-ws13.paedgymdon.local>
2017-06-27 13:01:08 +0200

    sun hinzugefügt

--- Garden.java
+++ Garden.java
@@ -4,6 +4,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Garden extends World
 {
     private MouseInfo mouse = Greenfoot.getMouseInfo();
+    private long lastFallingTime;
+    private int fallingSpeed;
     
     public Garden()
     {    
@@ -11,6 +13,14 @@ public class Garden extends World
     }
     
     public void act() {
-        
+        if(System.currentTimeMillis() > (lastFallingTime + fallingSpeed)) {
+            fall();
+            lastFallingTime = System.currentTimeMillis();
+        }
+    }
+    
+    public void fall() {
+        Sun s = new Sun();
+        addObject(s, 0, (50));
     }
 }
