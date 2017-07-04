9c5f242f22a38dc49b49f098456f8cfa5f079442
Felix Baier <baierfe@comw-ws10.paedgymdon.local>
2017-07-04 12:28:34 +0200

    Zombie Class

--- Zombie.java
+++ Zombie.java
@@ -8,12 +8,30 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Zombie extends Actor
 {
-    /**
-     * Act - do whatever the Zombie wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    private int hp;
+    private int walkspeed;
+    private int damage;
+    
+    public Zombie(int hp, int walkspeed, int damage)
+    {
+        this.hp = hp;
+        this.walkspeed = walkspeed;
+        this.damage = damage;
+    }
+    
+    
     public void act() 
     {
-        // Add your action code here.
-    }    
+      move(-6);
+      if(getX()<10)
+      {Greenfoot.stop();}
+     
+    } 
+    
+    public void s()
+    {
+      if(getObjectsInRange(40)=1)
+      { this.hp=hp-1;
+        }
+    }
 }
